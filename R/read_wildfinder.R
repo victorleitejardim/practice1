dcd164ee0a7188b718c7016da2b7d4cc88bfb3bd
Victor Leite Jardim <vleitejardim@gmail.com>
2022-11-30 12:18:28 +0100

    Add a new parameter to read_wildfinder and edit Exercise_3.Rmd

--- R/read_wildfinder.R
+++ R/read_wildfinder.R
@@ -1,12 +1,15 @@
 #' read_wildfinder
 #' a fuction to return a list with imported tables from all csv files read by fyunction read_csv from readr package
+#'
+#' @param fam select a single family from the databse
+#'
 #' @return
 #' @export
 #'
 #' @examples
 
 
-read_wildfinder <- function(){
+read_wildfinder <- function(fam = NULL){
   path <- here::here("data", "wildfinder")
   filenames <- list.files(path = path, full.names = TRUE)
   list_elem_names <- basename(filenames)
@@ -21,10 +24,17 @@ read_wildfinder <- function(){
   }
   #return(res)
   names(res) <- list_elem_names
-  res$wildfinder_mammals_list <- res$wildfinder_mammals_list |> 
-    dplyr::filter(family == "Ursidae")
-  final_table <- res$wildfinder_mammals_list |>
-    dplyr::left_join(res$wildfinder_ecoregions_species) |>
-    dplyr::left_join(res$wildfinder_ecoregions_list)
+  
+  if(is.null(fam)){
+    final_table <- res$wildfinder_mammals_list |>
+      dplyr::left_join(res$wildfinder_ecoregions_species) |>
+      dplyr::left_join(res$wildfinder_ecoregions_list)
+  } else{
+    res$wildfinder_mammals_list <- res$wildfinder_mammals_list |> 
+      dplyr::filter(family == fam)
+    final_table <- res$wildfinder_mammals_list |>
+      dplyr::left_join(res$wildfinder_ecoregions_species) |>
+      dplyr::left_join(res$wildfinder_ecoregions_list)
+    }
   final_table
 }
